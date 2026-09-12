f10f994b4a5b6ece71477cb66403b8d272bd0397
Mateus Tomomitsu <matomomitsu@gmail.com>
2026-09-11 23:22:27 -0300

    fix: bug04 - aplica desconto de vinte por cento em filmes

--- streamfiap-bughunt/streamfiap-bughunt/src/main/java/br/com/fiap/streamfiap/model/Filme.java
+++ streamfiap-bughunt/streamfiap-bughunt/src/main/java/br/com/fiap/streamfiap/model/Filme.java
@@ -22,7 +22,7 @@ public class Filme extends Conteudo implements Promocionavel {
 
     @Override
     public double aplicarPromocao(double preco) {
-        return preco * 1.2;
+        return Math.round(preco * 0.8 * 100.0) / 100.0;
     }
 
     public boolean isEstreia() { return estreia; }
